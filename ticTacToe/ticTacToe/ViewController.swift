8a0be03b683bb8e50a631d2417ce3082896c1ccc
anton8775 <yacenko372o@gmail.com>
2022-11-21 17:56:06 +0200

    Add startButton and boardButtons

--- ticTacToe/ticTacToe/ViewController.swift
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet var boardButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onStratButtonTouch(_ sender: UIButton) {
+        titleLabel.text = "Your turn"
+        startButton.isEnabled = false
+    }
+    @IBAction func onBoardButtonTouch(_ sender: UIButton) {
+        sender.setTitle("X", for: .normal)
+    }
+    
 }
 
