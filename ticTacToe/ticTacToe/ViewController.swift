360e98858e5457f4d7c53b83886d2bb0688c45c1
anton8775 <yacenko372o@gmail.com>
2022-11-21 17:02:30 +0200

    initial commit

---
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ticTacToe
+//
+//  Created by Антон Яценко on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
